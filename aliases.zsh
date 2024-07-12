0aace7944128fc48b457136b6369f1ab49c9c36e
Eric Bomgardner <ericbomgardner@users.noreply.github.com>
2024-07-12 10:56:16 -0400

    Add addtional aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,6 @@
 # ~/.zshrc and alias management
 alias openbash="open -t $ZSH_CUSTOM/aliases.zsh"
+alias catbash="cat $ZSH_CUSTOM/aliases.zsh"
 alias src="source ~/.zshrc"
 alias zsh="source ~/.oh-my-zsh/oh-my-zsh.sh"
 
@@ -13,6 +14,12 @@ gsa() {
   git --no-pager stash list | grep $(git rev-parse --abbrev-ref HEAD)
   git status
 }
+alias glo='git --no-pager log --format="%C(auto)%h %s" -n20'
+alias gsm="git --no-pager log origin/main..HEAD --reverse --pretty=tformat:'- %s'"
+alias gsc="git --no-pager log origin/main..HEAD --reverse --pretty=tformat:'- %s (%h)'"
+alias gb="git --no-pager branch"
+alias gsup=ggsup  # AKA git branch --set-upstream-to=origin/$(git_current_branch)
+alias gup="git up"
 
 # terminal
 alias clr="clear && gsa"
